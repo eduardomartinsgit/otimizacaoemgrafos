fe1e5390c6d3c6f75e1c767e5f3082d20fadfd7c
Eduardo Martins <eduardomartins.ti@hotmail.com>
2019-05-12 19:39:37 -0300

    - Criação do método para inserir arestas no grafo (Atualizando a matriz de adjacencias)

--- Trabalho.py
+++ Trabalho.py
@@ -7,9 +7,7 @@ class MatrizAdjGrafo:
                         grafoJSON = json.load(json_file)
                         self.v = len(grafoJSON['vertices'])
                         self.e = len(grafoJSON['arestas'])
-                        print (grafoJSON['nome'])
-                        print (self.v)
-                        print (self.e)
+
                         for i in range(self.v): #Criação da matriz e inicializando com 0
                                 self.m.append(self.v * [0])                        
                         return grafoJSON
@@ -26,8 +24,17 @@ class MatrizAdjGrafo:
                         print('')
 
 
+        def inserirAresta(self, grafoJSON):
+                arestas = grafoJSON['arestas']
+                for i in range(0, len(arestas)):
+                        print (arestas[i][0])
+                        print (arestas[i][1])
+                        self.m[int(arestas[i][0]) - 1][int(arestas[i][1]) - 1] = int(1)
+                        self.m[int(arestas[i][1]) - 1][int(arestas[i][0]) - 1] = int(1)
+
 #FIM DA MATRIZ DE ADJACENCIAS
 
-grafoMatriz = MatrizAdjGrafo()
-grafoMatriz.lerGrafoJSON('grafo.txt')
-grafoMatriz.imprimirGrafo(grafoMatriz.m)
+grafo = MatrizAdjGrafo()
+grafoJSON = grafo.lerGrafoJSON('grafo.txt')
+grafo.inserirAresta(grafoJSON)
+grafo.imprimirGrafo(grafo.m)
