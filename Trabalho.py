b1218e95ba57338a48c0f3697e2e44973f87931e
Eduardo Martins <eduardomartins.ti@hotmail.com>
2019-05-12 19:07:22 -0300

    - Criação do método para imprimir o grafo - Ajustes no método de leitura do arquivo para criar o grafo - Inicializando o grafo zerado

--- Trabalho.py
+++ Trabalho.py
@@ -4,14 +4,30 @@ class MatrizAdjGrafo:
 
         def lerGrafoJSON(self, nomeArquivo):
                 with open(nomeArquivo) as json_file:
-                        grafoJSON = json.load(json_file) 
-                        print (grafoJSON)
+                        grafoJSON = json.load(json_file)
+                        self.v = len(grafoJSON['vertices'])
+                        self.e = len(grafoJSON['arestas'])
+                        print (grafoJSON['nome'])
+                        print (self.v)
+                        print (self.e)
+                        for i in range(self.v): #Criação da matriz e inicializando com 0
+                                self.m.append(self.v * [0])                        
+                        return grafoJSON
         def __init__(self):
                 self.v = 0
                 self.e = 0
                 self.m = []
 
+
+        def imprimirGrafo(self,grafo):
+                for i in range(self.v):
+                        for j in range(self.v):
+                                print(grafo[i][j], end=' ') # end=' ' para o print aparecer no console como uma matriz
+                        print('')
+
+
 #FIM DA MATRIZ DE ADJACENCIAS
 
 grafoMatriz = MatrizAdjGrafo()
 grafoMatriz.lerGrafoJSON('grafo.txt')
+grafoMatriz.imprimirGrafo(grafoMatriz.m)
