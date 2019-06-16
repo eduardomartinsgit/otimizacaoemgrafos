397c3a327129114b8501f320caa9285fa426b03a
Eduardo Martins <eduardomartins.ti@hotmail.com>
2019-06-16 13:03:44 -0300

    Implementação do SLIDE 5
    - Implementação do Método de Busca do SLIDE 5 (MATRIZ ADJACENCIAS)

--- Trabalho.py
+++ Trabalho.py
@@ -32,45 +32,8 @@ class Pilha():
         return len(self.dados)	
 #FIM DA IMPLEMENTAÇÃO DE UMA PILHA COMO ESTRUTURA AUXILIAR		
 
-#INICIO DA MATRIZ DE ADJACENCIAS	
-class MatrizAdjGrafo:
 
-        def lerGrafoJSON(self, nomeArquivo):
-                with open(nomeArquivo) as json_file:
-                        grafoJSON = json.load(json_file)
-                        self.v = len(grafoJSON['vertices'])
-                        self.e = len(grafoJSON['arestas'])
-
-                        for i in range(self.v): #Criação da matriz e inicializando com 0
-                                self.m.append(self.v * [0])                        
-                        return grafoJSON
-        def __init__(self):
-                self.v = 0
-                self.e = 0
-                self.m = []
-
-
-        def imprimirGrafo(self,grafo):
-                for i in range(self.v):
-                        for j in range(self.v):
-                                print(grafo[i][j], end=' ') # Imprimir no console em forma de matriz
-                        print('')
-
-
-        def inserirAresta(self, grafoJSON):
-                arestas = grafoJSON['arestas']
-                for i in range(0, len(arestas)):
-                        self.m[int(arestas[i][0]) - 1][int(arestas[i][1]) - 1] = int(1)
-                        self.m[int(arestas[i][1]) - 1][int(arestas[i][0]) - 1] = int(1)
-
-        def removerAresta(self, linha, coluna):
-                self.m[linha][coluna]=0
-                self.m[coluna][linha]=0
-                self.e= self.e - 1
-
-#FIM DA MATRIZ DE ADJACENCIAS
-
-
+# INICIO DA LISTA DE ADJACENCIAS
 class ListaAdjGrafo:
         def __init__(self):
                 self.L = []
@@ -122,18 +85,73 @@ class ListaAdjGrafo:
 
 # FIM DA LISTA DE ADJACENCIAS
 
+
+
+#INICIO DA MATRIZ DE ADJACENCIAS	
+class MatrizAdjGrafo:
+
+        def lerGrafoJSON(self, nomeArquivo): #MÉTODO PARA LEITURA DO GRAFO NO FORMATO JSON
+                with open(nomeArquivo) as json_file:
+                        grafoJSON = json.load(json_file)
+                        self.v = len(grafoJSON['vertices'])
+                        self.e = len(grafoJSON['arestas'])
+
+                        for i in range(self.v): #Criação da matriz e inicializando com 0
+                                self.m.append(self.v * [0])
+                                self.visitado.append(False)
+                                self.descoberta.append(self.v * [False])
+                                self.explorado.append(self.v * [False])
+                        return grafoJSON
+        def __init__(self):
+                self.v = 0
+                self.e = 0
+                self.m = []
+                self.explorado = []
+                self.descoberta = []
+                self.visitado = []         
+
+
+        def imprimirGrafo(self,grafo): #MÉTODO PARA IMPRIMIR A ESTRUTURA DO GRAFO 
+                for i in range(self.v):
+                        for j in range(self.v):
+                                print(grafo[i][j], end=' ') # Imprimir no console em forma de matriz
+                        print('')
+
+
+        def inserirAresta(self, grafoJSON): #MÉTODO PARA INSERIR UMA NOVA ARESTA
+                arestas = grafoJSON['arestas']
+                for i in range(0, len(arestas)):
+                        self.m[int(arestas[i][0]) - 1][int(arestas[i][1]) - 1] = int(1)
+                        self.m[int(arestas[i][1]) - 1][int(arestas[i][0]) - 1] = int(1)
+
+        def removerAresta(self, linha, coluna): #MÉTODO PARA REMOÇÃO DE UMA ARESTA
+                self.m[linha][coluna] = 0
+                self.m[coluna][linha] = 0
+                self.e= self.e - 1
+
+        def Busca(self,r):#MÉTODO DE BUSCA DE UM DETERMINADO ELEMENTO NO GRAFO, SLIDE 5
+            self.visitado[r] = True
+            for x in range(self.v):
+                for y in range(self.v):
+                    if(self.visitado[x] and (not self.explorado[x][y])):
+                        self.explorado[x][y] = True
+                        if(not self.visitado[y]):
+                            self.visitado[y] = True
+                            self.descoberta[x][y] = True
+            
+				
+
+#FIM DA MATRIZ DE ADJACENCIAS
+
 grafo = MatrizAdjGrafo()
 grafoJSON = grafo.lerGrafoJSON('grafo.txt')
 grafo.inserirAresta(grafoJSON)
-#grafo.imprimirGrafo(grafo.m)
-#print ()
-#print ()
-grafo.removerAresta(1,2)
-#grafo.imprimirGrafo(grafo.m)
+grafo.imprimirGrafo(grafo.m)
+grafo.Busca(1)
 
 
 
-graph = ListaAdjGrafo()
-grafoJSON = graph.lerGrafoJSON('grafo.txt')
-graph.inserirAresta(grafoJSON)
-graph.imprimirVizinhos()
+#graph = ListaAdjGrafo()
+#grafoJSON = graph.lerGrafoJSON('grafo.txt')
+#graph.inserirAresta(grafoJSON)
+#graph.imprimirVizinhos()
